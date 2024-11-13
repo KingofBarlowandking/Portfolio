93f5592c216544c3146caa06f0722793118757bd
KingofBarlowandking <146796046+KingofBarlowandking@users.noreply.github.com>
2024-11-13 13:06:38 +1000

    try fix, should work, config file was generating server pages instead of static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
-import node from '@astrojs/node';
-
 export default defineConfig({
   integrations: [react()],
-  output: 'server',
-
-  adapter: node({
-    mode: 'standalone'
-  })
+  output: 'static',
 });
