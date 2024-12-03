63db65a5fe226c9d00c63ae48abe1abd39c4b2f9
KingofBarlowandking <146796046+KingofBarlowandking@users.noreply.github.com>
2024-12-03 16:24:00 +1000

    bold added for skills section, alt corrected in case study image

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,15 +10,15 @@ const Skills = ({ id }) => {
       <div className="skills-content">
         <div className="skills-text-box">
           <h2>Interpersonal</h2>
-          <p>Empathy - Strong intuition for a customers pain points and ways to improve on a design or product built through years of customer service.</p>
-          <p>Leadership and communication - Proven natural leadership and communication skills from past roles and other experiences.</p>
-          <p>Problem solving and creativity - Strong aptitude for developing ‘left field’ solutions and using quick decision making when required</p>
+          <p><b>Empathy</b> - Strong intuition for a customers pain points and ways to improve on a design or product built through years of customer service.</p>
+          <p><b>Leadership and communication</b> - Proven natural leadership and communication skills from past roles and other experiences.</p>
+          <p><b>Problem solving and creativity</b> - Strong aptitude for developing ‘left field’ solutions and using quick decision making when required</p>
         </div>
         <div className="skills-text-box">
           <h2>Technical</h2>
-          <p> Figma - Basic Figma skillset with experience designing wireframes and mockups for web and mobile. </p>
-          <p>Web development - Experience with html, css, React and Astro to build static websites and deploy them with Netlify.</p>
-          <p>Adobe - Use of Illustrator for basic vector graphics and custom typography, Indesign for presentations and Photoshop for limited photo editing work. </p>
+          <p><b>Figma</b> - Basic Figma skillset with experience designing wireframes and mockups for web and mobile. </p>
+          <p><b>Web development</b> - Experience with html, css, React and Astro to build static websites and deploy them with Netlify.</p>
+          <p><b>Adobe</b> - Use of Illustrator for basic vector graphics and custom typography, Indesign for presentations and Photoshop for limited photo editing work. </p>
         </div>
       </div>
     </section>
